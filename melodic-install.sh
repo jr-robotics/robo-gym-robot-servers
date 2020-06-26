ca1d5af06e72a1da8aaadba370b104c22b0c1836
Matteo Lucchi <matteo.lucchi@joanneum.at>
2020-06-26 14:53:24 +0200

    added python-catkin-tools installation to install files

--- melodic-install.sh
+++ melodic-install.sh
@@ -9,7 +9,7 @@ git clone https://github.com/jr-robotics/Universal_Robots_ROS_Driver.git
 
 cd ..
 sudo apt-get update
-sudo apt-get install -qq -y python-rosdep
+sudo apt-get install -qq -y python-rosdep python-catkin-tools
 sudo rosdep init
 rosdep update
 rosdep install --from-paths src -i -y --rosdistro melodic
