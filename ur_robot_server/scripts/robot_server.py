0dcb32ada923d391aa677da850a06afcfef55269
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-02-02 11:26:21 +0100

    formatted parameters in ur_robot/robot_server.py

--- ur_robot_server/scripts/robot_server.py
+++ ur_robot_server/scripts/robot_server.py
@@ -9,30 +9,30 @@ import yaml
 import os
 class RobotServerServicer(robot_server_pb2_grpc.RobotServerServicer):
     def __init__(self, real_robot, ur_model):
-        self.rosbridge = UrRosBridge(real_robot= real_robot, ur_model= ur_model)
+        self.rosbridge = UrRosBridge(real_robot=real_robot, ur_model=ur_model)
 
     def GetState(self, request, context):
         try:
             return self.rosbridge.get_state()
         except:
             logger.error('Failed to get state', exc_info=True)
-            return robot_server_pb2.State(success = 0)
+            return robot_server_pb2.State(success=0)
 
     def SetState(self, request, context):
         try:
-            self.rosbridge.set_state(state_msg = request)
-            return robot_server_pb2.Success(success =1)
+            self.rosbridge.set_state(state_msg=request)
+            return robot_server_pb2.Success(success=1)
         except:
             logger.error('Failed to set state', exc_info=True)
-            return robot_server_pb2.Success(success =0)
+            return robot_server_pb2.Success(success=0)
 
     def SendAction(self, request, context):
         try:
             executed_action = self.rosbridge.publish_env_arm_cmd(request.action)
-            return robot_server_pb2.Success(success =1)
+            return robot_server_pb2.Success(success=1)
         except:
             logger.error('Failed to send action', exc_info=True)
-            return robot_server_pb2.Success(success =0)
+            return robot_server_pb2.Success(success=0)
 
 def serve():
     initialize_logger()
@@ -42,7 +42,7 @@ def serve():
     ur_model = rospy.get_param("~ur_model")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     robot_server_pb2_grpc.add_RobotServerServicer_to_server(
-        RobotServerServicer(real_robot= real_robot, ur_model= ur_model), server)
+        RobotServerServicer(real_robot=real_robot, ur_model=ur_model), server)
     server.add_insecure_port('[::]:'+repr(server_port))
     server.start()
     if real_robot:
