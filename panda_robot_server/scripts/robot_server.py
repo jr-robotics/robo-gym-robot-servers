6417ff43fe89c35974b6cbb20e37660e2ff47927
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2021-03-26 13:04:34 +0100

    Added logger to robot_server package

--- panda_robot_server/scripts/robot_server.py
+++ panda_robot_server/scripts/robot_server.py
@@ -22,39 +22,37 @@ class RobotServerServicer(robot_server_pb2_grpc.RobotServerServicer):
             msg = robot_server_pb2.State()
             return self.rosbridge.get_state()
         except:
-            # logger.error('Failed to get state', exc_info=True)
-            return self._get_state_failure()
+            logger.error('Failed to get state', exc_info=True)
+            return robot_server_pb2.State(success=0)
 
     def SetState(self, request, context):
         try:
             self.rosbridge.set_state(state_msg=request)
-            return self._robot_server_get_success()
+            return robot_server_pb2.Success(success=1)
         except:
-            # logger.error('Failed to set state', exc_info=True)
-            return self._robot_server_get_failure()
+            logger.error('Failed to set state', exc_info=True)
+            return robot_server_pb2.Success(success=0)
 
     def SendAction(self, request, context):
         try:
             executed_action = self.rosbridge.publish_env_arm_cmd(request.action)
-            return self._robot_server_get_success()
+            return robot_server_pb2.Success(success=1)
         except:
-            # logger.error('Failed to send action', exc_info=True)
-            return self._robot_server_get_failure()
+            logger.error('Failed to send action', exc_info=True)
+            return robot_server_pb2.Success(success=0)
         
-    def _get_state_failure(self):
-        return robot_server_pb2.State(success=0)
-
-    def _robot_server_get_success(self):
-        return robot_server_pb2.Success(success=1)
-
-    def _robot_server_get_failure(self):
-        return robot_server_pb2.Success(success=0)
+        
+    def SendActionGetState(self, request, context):
+        try:
+            executed_action = self.rosbridge.publish_env_arm_cmd(request.action)
+            return self.rosbridge.get_state()
+        except:
+            logger.error('Failed to send action and get state', exc_info=True)
+            return robot_server_pb2.State(success=0)
 
 
 def serve():
-    # _initialize_logger()
-    # logger.info('Starting Panda Robot Server...')
-    rospy.loginfo('Starting Panda Robot Server...')
+    logger.info('Starting Panda Robot Server...')
     server_port = rospy.get_param('~server_port')
     real_robot = rospy.get_param('~real_robot', False)
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -63,10 +61,10 @@ def serve():
     server.start()
 
     if real_robot:
-        rospy.loginfo('Panda - Real Robot Server started at ' +
+        logger.info('Panda - Real Robot Server started at ' +
                       repr(server_port))
     else:
-        rospy.loginfo('Panda - Sim Robot Server started at ' +
+        logger.info('Panda - Sim Robot Server started at ' +
                       repr(server_port))
 
     rospy.spin()
@@ -74,15 +72,11 @@ def serve():
 
 def _initialize_logger():
     global logger
-    handlers_tag = 'handlers'
-    file_tag = 'file'
-    filename_tag = 'filename'
-
+    
     package_path = os.path.join(os.path.dirname(__file__), '..', '..')
-    with open(os.path.join(package_path, 'logging_config.yml'), 'r') as stream:
+    with open(os.path.join(package_path, 'panda_logging_config.yml'), 'r') as stream:
         config = yaml.safe_load(stream)
-    config[handlers_tag][file_tag][filename_tag] = os.path.join(
-        package_path, config[handlers_tag][file_tag][filename_tag])
+    config['handlers']['file']['filename'] = os.path.join(package_path, config['handlers']['file']['filename'] )
     logging.config.dictConfig(config)
     logger = logging.getLogger('panda_robot_server_logger')
 
@@ -90,10 +84,11 @@ def _initialize_logger():
 if __name__ == '__main__':
     try:
         wait_time = 5
+        _initialize_logger()
         rospy.init_node('robot_server')
-        rospy.loginfo('Waiting {}s before starting initialization of robot_server'.format(wait_time))
+        logger.info('Waiting {}s before starting initialization of robot_server'.format(wait_time))
         rospy.sleep(wait_time)
-        rospy.loginfo('Initializing robot_server node')
+        logger.info('Initializing robot_server node')
         serve()
     except(KeyboardInterrupt, SystemExit):
         pass
